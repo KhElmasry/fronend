0f8fe64aa7ca23b9b4d18fff077b3feffc508c80
ELMA <liv.bettterlife@gmail.com>
2023-01-31 22:31:26 +0200

    ssss

--- mint.js
+++ mint.js
@@ -1,5 +1,5 @@
 const Web3 = require('web3');
-const web3 = new Web3('4e79e9a2-b4ce-40ab-b3da-0f9459f0068c');
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
 const contractAddress = '0xF8402BE77548A298F6Ef979f5AAd90573cfC99A0';
 const contractABI = [{...}];
@@ -14,23 +14,6 @@ mintButton.addEventListener('click', () => {
       mintCount.innerHTML = parseInt(mintCount.innerHTML) + 1;
     })
     .catch(error => {
-
-      async function mintNFT() {
-        // Get the number of NFTs to mint
-        const nftCount = parseInt(document.getElementById("nft-count").value);
-      
-        // Call the contract to mint the NFTs
-        const result = await web3.eth.sendTransaction({
-          to: CONTRACT_ADDRESS,
-          value: nftCount * NFT_PRICE,
-          data: MINTER_ABI.encodeABI()
-        });
-      
-        // Update the number of mints displayed on the website
-        document.getElementById("mint-count").innerHTML = parseInt(document.getElementById("mint-count").innerHTML) + nftCount;
-      
-        // Show a success message
-        alert("NFTs minted successfully!");
-      }
-      
-      console.
+      console.error(error);
+    });
+});
