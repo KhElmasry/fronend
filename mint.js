10f30ae9adfdcfcedf3a7898c99b52df59e4ed3a
ELMA <liv.bettterlife@gmail.com>
2023-01-31 22:46:06 +0200

    sss

--- mint.js
+++ mint.js
@@ -2,7 +2,9 @@ const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
 const contractAddress = '0xF8402BE77548A298F6Ef979f5AAd90573cfC99A0';
-const contractABI = [{...}];
+const contractABI = [{
+  // your contract ABI goes here
+}];
 const contract = new web3.eth.Contract(contractABI, contractAddress);
 
 const mintButton = document.getElementById('mintButton');
